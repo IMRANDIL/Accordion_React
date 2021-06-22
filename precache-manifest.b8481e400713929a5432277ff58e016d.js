480d40bba9fbeb97425f2826c0c2c122745c915c
IMRANDIL <aliimranadil2@gmail.com>
2021-06-22 11:38:03 +0530

    Updates

---
+++ precache-manifest.b8481e400713929a5432277ff58e016d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ebbd91abf8a40f872a0de728f42d26",
+    "url": "/Accordion_React/index.html"
+  },
+  {
+    "revision": "f83fe7851009bb724e43",
+    "url": "/Accordion_React/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/Accordion_React/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Accordion_React/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83fe7851009bb724e43",
+    "url": "/Accordion_React/static/js/main.a026b5fe.chunk.js"
+  },
+  {
+    "revision": "dec4f566d38dcb63c6f2",
+    "url": "/Accordion_React/static/js/runtime-main.a5461449.js"
+  }
+]);
